e013f11a1d673494501f9021e39da4bb7e21bcd8
shreedeep <shreedeepsawaikar@gmail.com>
2020-04-11 22:07:06 +0530

    Initial dist subtree commit

---
+++ precache-manifest.0f63aedc5ded770d76bdaef0ec4408e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea907e05761e713ccd6a",
+    "url": "/vue-tour-of-heroes/css/app.3cf5ea12.css"
+  },
+  {
+    "revision": "79b2e8f54d5395713657ebe60a8a86e7",
+    "url": "/vue-tour-of-heroes/index.html"
+  },
+  {
+    "revision": "ea907e05761e713ccd6a",
+    "url": "/vue-tour-of-heroes/js/app.a732adc1.js"
+  },
+  {
+    "revision": "aaa2acb75b4232293f4a",
+    "url": "/vue-tour-of-heroes/js/chunk-vendors.b0154155.js"
+  },
+  {
+    "revision": "66ac0c9423cfd052639d21b7e17a7f60",
+    "url": "/vue-tour-of-heroes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-tour-of-heroes/robots.txt"
+  }
+]);
